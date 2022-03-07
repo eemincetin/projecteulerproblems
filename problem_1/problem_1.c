42ec61652111d47e596f24aa62b368fbdfd5bd8a
Emin Cetin <eeminceetin@gmail.com>
2022-03-07 13:51:26 +0300

    solution for problem_1

---
+++ problem_1/problem_1.c
@@ -0,0 +1,3 @@
+int multiplesOf3or5(int ival){
+    return ((ival > 0) ? ((((ival - 1) % 3 == 0) || ((ival - 1) % 5 == 0)) ? ((ival - 1) + multiplesOf3or5(ival - 1)) : multiplesOf3or5(ival - 1)) : 0); 
+}
