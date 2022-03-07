f727b4b9c782e867d711ac3e3a2ed6daca68364a
Emin Cetin <eeminceetin@gmail.com>
2022-03-07 13:55:23 +0300

    adding_link

--- problem_1/problem_1.c
+++ problem_1/problem_1.c
@@ -1,3 +1,5 @@
+// https://projecteuler.net/problem=1
+
 int multiplesOf3or5(int ival){
     return ((ival > 0) ? ((((ival - 1) % 3 == 0) || ((ival - 1) % 5 == 0)) ? ((ival - 1) + multiplesOf3or5(ival - 1)) : multiplesOf3or5(ival - 1)) : 0); 
 }
